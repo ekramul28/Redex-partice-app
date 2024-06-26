f1e2c8e5a1194d54558edba4134936630017c732
Ekramul <mdekramulhassan168@gmail.com>
2024-06-26 23:30:55 +0600

    redex or typescript with todo app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>My Button</Button>
+      <h1>My App</h1>
+    </>
+  );
+}
+
+export default App;
