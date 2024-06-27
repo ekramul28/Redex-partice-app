4f8c6b62c42efa6bf5132647dd2524d178b3680c
Ekramul <mdekramulhassan168@gmail.com>
2024-06-27 16:41:20 +0600

    todoFiter and todoContainer added

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,20 +1,23 @@
+import { Button } from "../ui/button";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
+      <div className="flex justify-between mb-5">
+        <Button className="bg-primary-gradient ">Add todo</Button>
         <button>Filter</button>
       </div>
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-5">
-        <div className="bg-white text-2xl font-bold p-3 flex justify-center items-center rounded-md">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-2 space-y-5">
+        {/* <div className="bg-white text-2xl font-bold p-3 flex justify-center items-center rounded-md">
           <p>There is no Task pending</p>
+        </div> */}
+        <div className="bg-white p-2 w-full h-full rounded-xl space-y-3 ">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
         </div>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
-        <TodoCard></TodoCard>
       </div>
     </div>
   );
